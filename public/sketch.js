83d7b2faf3df7763a049602ea454ab42e9778466
lucasfrmr <lucasfrmr@gmail.com>
2017-06-20 05:09:15 -0500

    Updated sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,56 @@
+
+// dude object
+function Dude(){
+  this.x = width/2;
+  this.y = height/2;
+
+  // draw dude
+  this.show = function(){
+    
+    fill(255, 0, 0);
+    //text("James", this.x - 21, this.y - 35); // name
+    rect(this.x, this.y, 55, 55); // head
+    
+    if(this.x < 0){this.x = width};
+    if(this.x > width){this.x = 0};
+    if(this.y < 0){this.y = height};
+    if(this.y > height){this.y = 0};
+  }
+
+  //Movement Controls
+  this.move = function(movementSpeed){
+    this.moveSpeed = movementSpeed;
+    if (keyIsDown(65))
+      this.x-=this.moveSpeed;
+    if (keyIsDown(68))
+      this.x+=this.moveSpeed;
+    if (keyIsDown(87))
+      this.y-=this.moveSpeed;
+    if (keyIsDown(83))
+      this.y+=this.moveSpeed;
+  }
+}
+
+// main sketch
+var movementSpeed;
+
 function setup(){
-	createCanvas(800, 600);
+	// bg = loadImage("#");
+	createCanvas(windowWidth, windowHeight);
+	rectMode(CENTER);
+	
+	movementSpeed = 5;
+
+	// create dude  
+  dude = new Dude();
 }
 
 function draw(){
-	ellipse(width/2, height/2, 50, 50);
+	// background(bg);
+	background(100);
+
+	// show & move dude methods
+	dude.show();
+  dude.move(movementSpeed);
+
 }
