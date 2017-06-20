f95dce192d0a81f42fc6c8f1a38f20a62ec16cc8
JamesTGH <jamesbruddick@gmail.com>
2017-06-20 03:59:40 -0500

    Configured server.js & index.html

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-// Using express: http://expressjs.com/
 var express = require('express');
-// Create the app
 var app = express();
 
-// This is for hosting files
-// Anything in the public directory will be served
-// This is just like python -m SimpleHTTPServer
-// We could also add routes, but aren't doing so here
-app.use(express.static('public'));
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
 
-// Set up the server
-// process.env.PORT is related to deploying on heroku
-var server = app.listen(process.env.PORT || 3000, listen);
-
-// This call back just tells us that the server has started
-function listen() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
-}
+app.listen(app.get('port'), function() {
+  console.log('Server started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
